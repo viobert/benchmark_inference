1abcce358c22850b8cbf0ee70fe0a71553f7bf40
MKX <bettermkx@163.com>
2026-01-17 18:33:16 +0800

    [fix]: fix metrics label type incompatible

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,23 +1,36 @@
 from typing import Dict, Iterable, Optional, Union
 
 
+def _coerce_optional_bool(value: Optional[Union[bool, int]]) -> Optional[bool]:
+    # Accept 0/1 or bool; return None for anything else.
+    if value is None:
+        return None
+    if isinstance(value, bool):
+        return value
+    if isinstance(value, int) and value in (0, 1):
+        return bool(value)
+    return None
+
+
 def compute_binary_metrics(
-    labels: Iterable[Optional[bool]],
-    preds: Iterable[Optional[bool]],
+    labels: Iterable[Optional[Union[bool, int]]],
+    preds: Iterable[Optional[Union[bool, int]]],
 ) -> Dict[str, Union[int, float]]:
     tp = tn = fp = fn = 0
     valid = 0
     for label, pred in zip(labels, preds):
-        if label is None or pred is None:
+        label_bool = _coerce_optional_bool(label)
+        pred_bool = _coerce_optional_bool(pred)
+        if label_bool is None or pred_bool is None:
             continue
         valid += 1
-        if label is True and pred is True:
+        if label_bool is True and pred_bool is True:
             tp += 1
-        elif label is False and pred is False:
+        elif label_bool is False and pred_bool is False:
             tn += 1
-        elif label is False and pred is True:
+        elif label_bool is False and pred_bool is True:
             fp += 1
-        elif label is True and pred is False:
+        elif label_bool is True and pred_bool is False:
             fn += 1
 
     acc = (tp + tn) / valid if valid else 0.0
